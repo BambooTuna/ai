2397be7a1e0098c436c5f00b48be5c5f9588f8e5
BambooTuna <bambootuna@gmail.com>
2022-08-02 18:32:13 +0900

    :fix: image loader

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,16 +1,22 @@
 import glob
 from torch.utils.data import Dataset
+from PIL import Image
+
+
+def pil_loader(path):
+    return Image.open(path).convert("RGB")
 
 
 class DatasetFolderPairs(Dataset):
     def __init__(self, image_dir, data_transforms, common_transform):
-        super(DatasetFolderPairs, self).__init__()
-        self.filenames = glob.glob(f"{image_dir}/*.jpg", recursive=True)
+        super().__init__()
+        self.filenames = glob.glob(f"{image_dir}/**/*.jpg", recursive=True)
         self.data_transforms = data_transforms
         self.common_transform = common_transform
 
     def __getitem__(self, index):
-        return [self.common_transform(data_transform(self.filenames[index])) for data_transform in self.data_transforms]
+        img = pil_loader(self.filenames[index])
+        return tuple([self.common_transform(data_transform(img)) for data_transform in self.data_transforms])
 
     def __len__(self):
         return len(self.filenames)
